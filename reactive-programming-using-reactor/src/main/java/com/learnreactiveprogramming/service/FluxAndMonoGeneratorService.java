f21de53b2162057ebd4f2c6e2a8c68d50fa98b0c
Josh <jdh428@gmail.com>
2022-09-20 00:30:30 -0400

    First flux and mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,29 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("alex");
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> {
+                    System.out.println("Name is " + name);
+                });
+        fluxAndMonoGeneratorService.namesMono().subscribe(name -> {
+            System.out.println("Mono Name is " + name);
+        });
+
+    }
 }
